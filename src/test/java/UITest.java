3993a844e3329c749eb70e6ab92e05c4cd798732
Sidiqa Fekrat <Sfekrat@depaul.edu>
2025-11-12 00:33:50 -0600

    Launch browser in headless mode based on CI environment

--- src/test/java/UITest.java
+++ src/test/java/UITest.java
@@ -20,12 +20,12 @@ public class UITest {
     @BeforeEach
     public void setUp() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
-                .setHeadless(false));
+        boolean isCI = System.getenv("CI") != null || System.getenv("GITHUB_ACTIONS") != null;
+    
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(isCI)); 
+        
         context = browser.newContext();
-        context = browser.newContext(new Browser.NewContextOptions()
-                .setRecordVideoDir(Paths.get("videos/"))
-                .setRecordVideoSize(1280, 720));
+        context = browser.newContext(new Browser.NewContextOptions().setRecordVideoDir(Paths.get("videos/")).setRecordVideoSize(1280, 720));
         page = context.newPage();
     }
 
